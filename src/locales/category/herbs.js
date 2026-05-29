32e15bfd09ebc22aa49140578b524f79db0cf58a
vigo <wugzman@gmail.com>
2026-05-29 22:05:37 +0800

    词条更新

--- src/locales/category/herbs.js
+++ src/locales/category/herbs.js
@@ -3,15 +3,17 @@
  * 由 npm run merge-transition 自动生成，请勿手改
  */
 window.DARKTRANS_CATEGORY_HERBS = {
-  "Phantom Flower": "幻影花",
-  "Rubysilver Ore": "红银矿石",
-  "Tidestone Ore": "潮石矿",
-  "Obsidian Ore": "黑曜石矿石",
+  "Rubysilver Ore": "红宝石银矿",
+  "Tidestone Ore": "潮汐石矿",
+  "Obsidian Ore": "黑曜石矿",
+  "Cobalt Ore": "钴矿",
+  "Froststone Ore": "霜石矿",
+  "Copper Ore": "铜矿",
+  "Gold Ore": "金矿",
+  "Iron Ore": "铁矿",
   "Black Rose": "黑玫瑰",
-  "Cobalt Ore": "钴矿石",
-  "Gold Ore": "金矿石",
-  "Iron Ore": "铁矿石",
   "Lifeleaf": "生命之叶",
+  "Phantom Flower": "幻影花",
   "Saltvine": "盐藤",
-  "Wardweed": "护卫草"
+  "Wardweed": "护符草"
 };
